017abbf249916654193d53756bfc8abc9203e6e4
User <User@WIN10>
2022-11-19 12:48:48 +0300

    Добавьте файлы проекта.

---
+++ Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcingYamaha
+{
+    internal class Card
+    {
+        public object Title { get;  set; }
+        public object Price { get;  set; }
+
+        internal void Parse(string response)
+        {
+            Console.WriteLine(response);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using Newtonsoft.Json;
+using RestSharp;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
+
+namespace ParcingYamaha
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+           
+            HttpClient httpClient = new HttpClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://parts.yamaha-motor.co.jp/ypec_b2c/services/html5/product_list/");
+            request.Content = new StringContent("{\"baseCode\":\"7306\",\"langId\":\"92\"}", Encoding.UTF8, "application/json");
+            request.Headers.Add("accept", "application/json, text/javascript, */*; q=0.01");
+            request.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.0.2419 Yowser/2.5 Safari/537.36");
+            //request.Headers.Add("", "");
+
+         
+
+
+            while (true)
+            {
+                string line = Console.ReadLine(); // Get string from user
+                if (line == "exit") // Check string
+                {
+                    break;
+                }
+                var response = await httpClient.SendAsync(request);
+                var answer = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(answer);
+
+            }
+            
+
+            Console.ReadKey();
+        
+
+        }
+    }
+}
